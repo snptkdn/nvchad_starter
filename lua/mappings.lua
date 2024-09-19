e6f6bee74b1b642964fdcc60cb8ec6c8951b800d
snptkdn <400m.wflick@gmail.com>
2024-09-19 16:06:17 +0900

    基本をセットアップした。

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
+map("n", "<C-f>", "<cmd>NvimTreeToggle<cr>")
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
