bc584be85135b032d2fd972b752e1a39c7816840
snptkdn <snptkdn@gmail.com>
2026-01-18 21:26:13 +0900

    tabを標準でspaceにした

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
